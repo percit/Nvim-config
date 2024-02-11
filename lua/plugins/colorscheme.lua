b157a565479faab5c94c06877622ed8fa97dbecd
Marcin Zdunek <marcin.zdunek@siili.com>
2024-02-11 14:45:57 +0100

    Add coloscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,63 @@
+return {
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				highlight_overrides = {
+					mocha = function(mocha)
+						return {
+							LineNr = { fg = mocha.overlay1 },
+						}
+					end,
+				},
+			})
+		end,
+		opts = {
+			flavour = "mocha",
+			integrations = {
+				aerial = true,
+				alpha = true,
+				cmp = true,
+				dashboard = true,
+				flash = true,
+				gitsigns = true,
+				harpoon = true,
+				headlines = true,
+				illuminate = true,
+				indent_blankline = { enabled = true },
+				leap = true,
+				lsp_trouble = true,
+				mason = true,
+				markdown = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				navic = { enabled = true, custom_bg = "lualine" },
+				neotest = true,
+				neotree = true,
+				noice = true,
+				notify = true,
+				semantic_tokens = true,
+				telescope = true,
+				treesitter = true,
+				treesitter_context = true,
+				which_key = true,
+			},
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
+}
